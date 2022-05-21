48e514f64e373e8b937bcbb296b1873e38a11ef2
CWIN77 <cwstbp0426@gmail.com>
2022-05-21 15:24:49 +0900

    My custom react ts

---
+++ src/firebase/firestore.ts
@@ -0,0 +1,3 @@
+import firebase from "./config";
+
+const db = firebase.firestore();
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components' 
+
+const Container = styled.div`
+  width:100%;
+  min-height:100%;
+` 
+
+function Home() {
+  return (
+    <Container>
+      App
+    </Container>
+  )
+}
+
+export default Home
